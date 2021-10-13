2ee7b81531c1c977b99416f2f8fbfb46e8545687
Ruslan Galimzyanov <ruska1812@gmail.com>
2021-10-13 21:26:02 +0500

    navbar + tips component

--- calorie-zen/src/components/App.js
+++ calorie-zen/src/components/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import AppContainer from './AppContainer';
+import Diary from './Diary';
+import Tips from './Tips';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './NavBar';
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <AppContainer />
-    </>
+      <main className="content">
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Diary />
+          </Route>
+          <Route path="/tips">
+            <Tips />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ calorie-zen/src/components/Tips.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import './Tips.css';
+
+function Tips() {
+  useEffect(() => {
+    fetch('https://api.nomoreparties.co/todays-tips-rus').then((res) => {
+      return res.json();
+    }).then((res) => {
+      setList(Object.values(res));  
+    })
+  }, []);
+  const [list, setList] = useState();
+  return (
+    <div className="tips">
+      <ul className="tips__list">
+      {list &&  
+        list.map((item) => (<li className="tips__item" key={item.tip}>{item.tip}</li>))
+      }
+      </ul>
+    </div>
+  );
+}
+
+export default Tips;
